0de8de384f3596ea217f4a3d8ed8b16dc714d596
web.ljg <web.ljg@gmail.com>
2022-07-19 11:00:59 +0900

    refactor: Redux를 사용해서 counter 구현

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,23 +1,29 @@
+import { createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
+number.innerText = 0; // 초기값 표시
 
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+// reducer
+const countModifier = (count = 0, action) => {
+  if (action.type === 'ADD') {
+    return count + 1;
+  } else if (action.type === 'MINUS') {
+    return count - 1;
+  } else {
+    return;
+  }
 };
 
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-};
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
+const countStore = createStore(countModifier); // store 생성, reducer는 countModifer다.
+
+const onChange = () => {
+  number.innerText = countStore.getState(); // 변경된 state를 number에 표시한다.
 };
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+countStore.subscribe(onChange); // data 변경을 감지할때마다 onChange를 실행시킨다.
+
+add.addEventListener('click', () => countStore.dispatch({ type: 'ADD' }));
+minus.addEventListener('click', () => countStore.dispatch({ type: 'MINUS' }));
