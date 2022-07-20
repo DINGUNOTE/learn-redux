8d1b09d67b06597063367b65e14f14490bfd4112
web.ljg <web.ljg@gmail.com>
2022-07-20 15:53:24 +0900

    refactor: createAction으로 action 간소화, createRudcer로 reducer 간소화, configureStore 적용

--- react-redux/src/store/index.js
+++ react-redux/src/store/index.js
@@ -1,40 +1,22 @@
 import { createStore } from 'redux';
+import { createAction, createReducer, configureStore } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const REMOVE = 'REMOVE';
+export const addToDo = createAction('ADD');
+export const removeToDo = createAction('REMOVE');
 
-export const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-  };
-};
+const reducer = createReducer(JSON.parse(localStorage.getItem('todos')) || [], {
+  [addToDo]: (state, action) => {
+    const addItem = [{ text: action.payload, id: Date.now() }, ...state];
+    localStorage.setItem('todos', JSON.stringify(addItem));
+    return addItem;
+  },
+  [removeToDo]: (state, action) => {
+    const removeItem = state.filter((toDo) => toDo.id !== action.payload);
+    localStorage.setItem('todos', JSON.stringify(removeItem));
+    return removeItem;
+  },
+});
 
-export const removeToDo = (id) => {
-  return {
-    type: REMOVE,
-    id: parseInt(id),
-  };
-};
-
-const reducer = (
-  state = JSON.parse(localStorage.getItem('todos')) || [],
-  action
-) => {
-  switch (action.type) {
-    case ADD:
-      const addItem = [{ text: action.text, id: Date.now() }, ...state];
-      localStorage.setItem('todos', JSON.stringify(addItem));
-      return addItem;
-    case REMOVE:
-      const removeItem = state.filter((toDo) => toDo.id !== action.id);
-      localStorage.setItem('todos', JSON.stringify(removeItem));
-      return removeItem;
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 export default store;
